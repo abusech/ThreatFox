17d08a0aa85940a6f0d3feae7c274aea0b291044
abuse.ch <41051806+abusech@users.noreply.github.com>
2024-11-09 17:31:10 +0100

    Update threatfox_query_recent-iocs.py

--- threatfox_query_recent-iocs.py
+++ threatfox_query_recent-iocs.py
@@ -4,16 +4,23 @@ import urllib3
 import json
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python3 threatfox_query_recent-iocs.py <number-of-days>")
+if len(sys.argv) < 3:
+    print("Script to query ThreatFox for recent IOCs")
+    print("Usage: python3 threatfox_query_recent-iocs.py <AUTH-KEY> <number-of-days>")
+    print("Note: If you don't have an Auth-Key yet, you can obtain one at https://auth.abuse.ch/")
     quit()
 
-pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50)
+headers = {
+    "Auth-Key"      :   sys.argv[1]
+}
+
+pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50, headers=headers)
 
 data = {
     'query':    'get_iocs',
-    'days':     int(sys.argv[1])
+    'days':     int(sys.argv[2])
 }
+
 json_data = json.dumps(data)
 response = pool.request("POST", "/api/v1/", body=json_data)
 response = response.data.decode("utf-8", "ignore")
