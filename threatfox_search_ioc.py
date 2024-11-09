9858cbe5ba6cf89d82e5bf7cbb4f72eb12b92f2f
abuse.ch <41051806+abusech@users.noreply.github.com>
2024-11-09 17:34:33 +0100

    Update threatfox_search_ioc.py

--- threatfox_search_ioc.py
+++ threatfox_search_ioc.py
@@ -4,15 +4,23 @@ import urllib3
 import json
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
+    print("Script to search ThreatFox for an IOC (e.g. URL, domain, file hash)")
     print("Usage: python3 threatfox_search_ioc.py <search-term>")
+    print("Note: If you don't have an Auth-Key yet, you can obtain one at https://auth.abuse.ch/")
     quit()
 
-pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50)
+
+headers = {
+    "Auth-Key"      :   sys.argv[1]
+}
+
+pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50, headers=headers)
+
 
 data = {
     'query':            'search_ioc',
-    'search_term':      sys.argv[1]
+    'search_term':      sys.argv[2]
 }
 json_data = json.dumps(data)
 response = pool.request("POST", "/api/v1/", body=json_data)
