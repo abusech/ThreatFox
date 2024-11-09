72cffe78a22df562e813e2aaa3dd9c47c0b4a2e2
abuse.ch <41051806+abusech@users.noreply.github.com>
2024-11-09 17:33:03 +0100

    Update threatfox_query_tag.py

--- threatfox_query_tag.py
+++ threatfox_query_tag.py
@@ -4,15 +4,21 @@ import urllib3
 import json
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
+    print("Script to query ThreatFox for Indicators Of Compromise (IOCs) associated with a specific tag")
     print("Usage: python3 threatfox_query_tag.py <tag>")
+    print("Note: If you don't have an Auth-Key yet, you can obtain one at https://auth.abuse.ch/")
     quit()
 
-pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50)
+headers = {
+    "Auth-Key"      :   sys.argv[1]
+}
+
+pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50, headers=headers)
 
 data = {
     'query':            'taginfo',
-    'tag':              sys.argv[1]
+    'tag':              sys.argv[2]
 }
 json_data = json.dumps(data)
 response = pool.request("POST", "/api/v1/", body=json_data)
